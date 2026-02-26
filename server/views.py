fc9756b51127b8f681b183874a00d95bad971d27
Daniel Venturini <danielventurini021@gmail.com>
2026-02-26 08:36:38 -0300

    feat(server): redirect outdated server/disk-usage to server/run/df

--- server/views.py
+++ server/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 
-# Create your views here.
 def time_viewer(request: HttpRequest) -> HttpResponse:
     if not request.headers.get('Bypass') or not request.headers.get('Bypass') == request.path:
         httpResponse = HttpResponse('Error!')
@@ -15,10 +14,6 @@ def time_viewer(request: HttpRequest) -> HttpResponse:
 
 
 def disk_usage_viewer(request: HttpRequest) -> HttpResponse:
-    # if not request.headers.get('Bypass') or not request.headers.get('Bypass') == request.path:
-    #     httpResponse = HttpResponse('Error!')
-    #     httpResponse.status_code = 403
-    #     return httpResponse
 
     import subprocess
 
@@ -27,7 +22,7 @@ def disk_usage_viewer(request: HttpRequest) -> HttpResponse:
         'output': subprocess.getoutput('df -h').split('\n')
     }
 
-    return render(request, 'default-response.html', {'context': context})
+    return HttpResponseRedirect('/server/run/df?args=-h')
 
 def run_command_viewer(request: HttpRequest, command: str) -> HttpResponse:
     import subprocess
