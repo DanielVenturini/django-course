2f529a533651cc9fdeddb7ad6e9bd36d1b91c1ea
Daniel Venturini <danielventurini021@gmail.com>
2026-06-21 16:54:35 -0300

    feat(project): add terraform code

---
+++ terraform/envs/stage-terraform.tfvars
@@ -0,0 +1,3 @@
+region                            = "us-central1"
+artifact_registry_repository_name = "django-app"
+gcp_credentials_file              = ".env/staging-sa.json"
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "artifact_registry_repository_name" {
+  type = string
+}
+
+variable "gcp_credentials_file" {
+  type = string
+}
