4a143714e7102979de502dee08d86492538c114b
Daniel Venturini <danielventurini021@gmail.com>
2026-06-27 20:26:52 -0300

    feat(settings): use correct run service URL

--- application/settings.py
+++ application/settings.py
@@ -28,7 +28,7 @@ from os import environ
 
 DEBUG = environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['djangoapp-693607810326.us-central1.run.app' if not DEBUG else '*']
+ALLOWED_HOSTS = ['production-app-django-run-service-693607810326.us-central1.run.app' if not DEBUG else 'stage-app-django-run-service-693607810326.us-central1.run.app,*']
 
 
 # Application definition
